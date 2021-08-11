9faff687804682ad957140efa011812390bb5b32
pavlo.hrishyn <pashagrishyn@gmai.com>
2021-08-11 18:41:07 +0300

    moved seed.json to root

--- src/db/db.py
+++ src/db/db.py
@@ -5,7 +5,7 @@ import json
 def get_db(configuration: AppSettings):
     client = MongoClient(configuration.MONGO_CONNECTION_STRING).get_default_database()
     if configuration.MONGO_FINANCIAL_CATEGORY_COLLECTION not in client.list_collection_names():
-        with open('./db/seed.json', encoding="utf-8") as seed_json:
+        with open('seed.json', encoding="utf-8") as seed_json:
             seed = json.load(seed_json)
             client[configuration.MONGO_FINANCIAL_CATEGORY_COLLECTION].insert_many(seed["categories"])
 
